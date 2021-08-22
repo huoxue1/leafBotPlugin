24e5ce80930e30e1b1932cbc442894a6d13e50c2
3343780376 <3343780376@qq.com>
2021-08-22 16:59:34 +0800

    群管插件增加内容 微博热搜插件可自动生成字体文件路径

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.7.0
 	github.com/fogleman/gg v1.3.0
 	github.com/google/go-github/v35 v35.2.0
-	github.com/huoxue1/leafBot v1.0.23
+	github.com/huoxue1/leafBot v1.0.24
 	github.com/jonboulle/clockwork v0.2.2 // indirect
 	github.com/mxschmitt/playwright-go v0.1100.0
 	github.com/shirou/gopsutil v3.21.4+incompatible
--- pluginGroupManager/manager.go
+++ pluginGroupManager/manager.go
@@ -41,7 +41,69 @@ func Init() {
 				}
 				bot.SetGroupAdmin(event.GroupId, ID, false)
 				nickName := bot.GetGroupMemberInfo(event.GroupId, ID, true).NickName
-				bot.Send(event, message.Text(nickName+"升为了管理！"))
+				bot.Send(event, message.Text(nickName+"失去了管理员的资格！"))
+			},
+		)
+
+	leafBot.OnRegex(`^踢出群聊.*?qq=(\d+)`).
+		SetPluginName("群管系统-踢出群聊").
+		SetBlock(false).
+		AddRule(leafBot.OnlySuperUser).
+		SetWeight(10).
+		AddHandle(
+			func(event leafBot.Event, bot *leafBot.Bot, state *leafBot.State) {
+				ID, err := strconv.Atoi(state.RegexResult[1])
+				if err != nil {
+					bot.Send(event, message.Text("发生未知错误"+err.Error()))
+					return
+				}
+				bot.SetGroupKick(event.GroupId, ID, false)
+				nickName := bot.GetGroupMemberInfo(event.GroupId, ID, true).NickName
+				bot.Send(event, message.Text(nickName+"被移除了群聊！"))
+			},
+		)
+
+	leafBot.OnRegex(`^退出群聊.*?(\d+)`).
+		SetPluginName("群管系统-退出群聊").
+		SetBlock(false).
+		AddRule(leafBot.OnlySuperUser).
+		SetWeight(10).
+		AddHandle(
+			func(event leafBot.Event, bot *leafBot.Bot, state *leafBot.State) {
+				ID, err := strconv.Atoi(state.RegexResult[1])
+				if err != nil {
+					bot.Send(event, message.Text("发生未知错误"+err.Error()))
+					return
+				}
+				bot.SetGroupLeave(ID, true)
+				nickName := bot.GetGroupMemberInfo(event.GroupId, ID, true).NickName
+				bot.Send(event, message.Text(nickName+"被移除了群聊！"))
+			},
+		)
+
+	leafBot.OnCommand(`开启全员禁言`).
+		SetPluginName("群管系统-全体禁言").
+		SetBlock(false).
+		AddRule(leafBot.OnlySuperUser).
+		SetWeight(10).
+		AddHandle(
+			func(event leafBot.Event, bot *leafBot.Bot, state *leafBot.State) {
+
+				bot.SetGroupWholeBan(event.GroupId, true)
+				bot.Send(event, message.Text("全员开始自闭"))
+			},
+		)
+
+	leafBot.OnCommand(`解除全员禁言`).
+		SetPluginName("群管系统-关闭全员群聊").
+		SetBlock(false).
+		AddRule(leafBot.OnlySuperUser).
+		SetWeight(10).
+		AddHandle(
+			func(event leafBot.Event, bot *leafBot.Bot, state *leafBot.State) {
+
+				bot.SetGroupWholeBan(event.GroupId, false)
+				bot.Send(event, message.Text("全员自闭结束"))
 			},
 		)
 
