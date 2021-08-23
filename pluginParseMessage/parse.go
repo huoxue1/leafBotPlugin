86292c62df484288ec12fc7352a329e38bdc796e
3343780376 <3343780376@qq.com>
2021-08-23 21:26:29 +0800

    添加复杂消息解析插件

---
+++ pluginParseMessage/parse.go
@@ -0,0 +1,27 @@
+package pluginParseMessage
+
+import (
+	"github.com/huoxue1/leafBot"
+	"github.com/huoxue1/leafBot/message"
+)
+
+func init() {
+	InitParse()
+}
+
+func InitParse() {
+	leafBot.OnCommand("decode").SetWeight(10).SetPluginName("消息解析").SetBlock(false).SetCD("default", 5).AddHandle(func(event leafBot.Event, bot *leafBot.Bot, state *leafBot.State) {
+		bot.Send(event, message.Text("请在一分钟内发出需要解析的消息"))
+		oneEvent, err := bot.GetOneEvent(func(event1 leafBot.Event, bot *leafBot.Bot, state *leafBot.State) bool {
+			if event1.GroupId == event.GroupId && event1.UserId == event.UserId {
+				return true
+			}
+			return false
+		})
+		if err != nil {
+			bot.Send(event, message.Text("等待超时"))
+			return
+		}
+		bot.Send(event, message.Text(oneEvent.Message.CQString()))
+	})
+}
