2443405e2edc9fa9461730440c9cb36f0170349c
3343780376 <3343780376@qq.com>
2021-09-18 22:40:13 +0800

    ci: 添加自动化服务器部署

--- quick_deploy.sh
+++ quick_deploy.sh
@@ -1,7 +1,7 @@
 pkill main
 git checkout .
 git pull
-/usr/local/go/bin/go mod tidy
-/usr/local/go/bin/go build main.go
+echo $(date +%F%n%T) > log.log
+go mod tidy
+go build main.go
 nohup ./main > bot.log 2>&1 &
-echo $(date +%F%n%T) > do.log
