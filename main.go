5859e4ec9f3dc0a698f65bf2663fb255dce38732
3343780376 <3343780376@qq.com>
2022-01-06 21:37:02 +0800

    init code

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/huoxue1/leafbot"
-	"github.com/huoxue1/leafbot/driver/cqhttp_reverse_ws_driver"
+	"github.com/huoxue1/leafbot/driver/cqhttp_default_driver"
 
 	_ "github.com/huoxue1/fan/plugin/group-mamanger"
 	_ "github.com/huoxue1/fan/plugin/sign"
 )
 
 func main() {
-	driver := cqhttp_reverse_ws_driver.NewDriver()
+	driver := cqhttp_default_driver.NewDriver()
 	leafbot.LoadDriver(driver)
 	leafbot.InitBots()
 	driver.Run()
