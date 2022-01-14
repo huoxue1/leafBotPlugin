2a65cee7eaded2b9739eabd199920262d7aa5cda
3343780376 <3343780376@qq.com>
2022-01-14 18:39:06 +0800

    feat: 添加积分查询

--- plugin/sign/sign.go
+++ plugin/sign/sign.go
@@ -23,6 +23,22 @@ func do() {
 	}).Handle(func(ctx *leafbot.Context) {
 		sign(ctx)
 	})
+
+	plugin.OnFullMatchGroup("积分查询", leafbot.Option{
+		Weight: 10,
+		Block:  true,
+		Allies: []string{"我的积分"},
+	}).Handle(func(ctx *leafbot.Context) {
+		s := new(Sign)
+		s.QQ = int64(ctx.Event.UserId)
+		err := query(s)
+		if err != nil {
+			return
+		}
+		ctx.Send([]message.MessageSegment{
+			message.Text(fmt.Sprintf("你当前积分为%d,已连续签到%d天", s.Fraction, s.ContinueSign)),
+			message.At(int64(ctx.Event.UserId))})
+	})
 }
 
 func sign(ctx *leafbot.Context) {
@@ -39,7 +55,7 @@ func sign(ctx *leafbot.Context) {
 		ctx.Send(message.Text("今日已经签过到了额，请明日再来吧"))
 		return
 	}
-	n := rand.Int63n(3)
+	n := rand.Int63n(3) + 1
 	s.Fraction += int64(n)
 	if isYesterday(s.LastSign) {
 		s.ContinueSign++
@@ -53,7 +69,7 @@ func sign(ctx *leafbot.Context) {
 	update(*s)
 	ctx.Send(append(message.Message{},
 		//message.Image(fmt.Sprintf("http://q1.qlogo.cn/g?b=qq&nk=%d&s=640", ctx.Event.UserId)).Add("cache", 0),
-		message.Text(fmt.Sprintf("恭喜你，签到成功,积分增加%d,当前共有积分%d,已连续签到%d", n, s.Fraction, s.ContinueSign)),
+		message.Text(fmt.Sprintf("恭喜你，签到成功,积分增加%d,当前共有积分%d,已连续签到%d天", n, s.Fraction, s.ContinueSign)),
 		message.At(int64(ctx.Event.UserId))))
 
 }
