6977d7d96698fbeb59f301ba9af9401b942b3cc0
3343780376 <3343780376@qq.com>
2022-01-26 12:27:39 +0800

    feat: 添加gif插件

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"github.com/huoxue1/leafbot"
-	"github.com/huoxue1/leafbot/driver/cqhttp_default_driver"
+	"github.com/huoxue1/leafbot/driver/cqhttp_reverse_ws_driver"
 
+	_ "github.com/huoxue1/fan/plugin/gif"
 	_ "github.com/huoxue1/fan/plugin/group-file"
 	_ "github.com/huoxue1/fan/plugin/group-mamanger"
 	_ "github.com/huoxue1/fan/plugin/sign"
 )
 
 func main() {
-	driver := cqhttp_default_driver.NewDriver()
+	driver := cqhttp_reverse_ws_driver.NewDriver()
 	leafbot.LoadDriver(driver)
 	leafbot.InitBots()
 	driver.Run()
